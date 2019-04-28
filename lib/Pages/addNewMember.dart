759bcd7cbc8daf5a46f018dfa4bb09880cbf9412
pathum <awpathum@gmail.com>
2019-04-28 16:07:32 +0530

    3rd commit

--- lib/Pages/addNewMember.dart
+++ lib/Pages/addNewMember.dart
@@ -26,7 +26,7 @@ class _addNewMemberState extends State<addNewMember> {
                     overflow: TextOverflow.ellipsis,
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  
+
                 ],
               ),
             )
