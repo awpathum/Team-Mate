03f845340faff94167f36ac5dc024128d819fd02
pathum <awpathum@gmail.com>
2019-05-03 22:48:08 +0530

    Create Sheet is working without search

--- lib/Pages/createSheet.dart
+++ lib/Pages/createSheet.dart
@@ -1,21 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:teamapp/Pages/addNewMember.dart';
-import 'package:flutter_calendar/flutter_calendar.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:teamapp/Services/ListPage.dart';
 
 class CreateSheet extends StatefulWidget {
-  //static String get strDate => strDate;
-
-
-
- // static get uploadData => CreateSheet.uploadData;
-
   @override
   CreateSheetState createState() => CreateSheetState();
 }
+
 List<String> names = new List<String>();
 List<String> index = new List<String>();
 
@@ -71,12 +62,6 @@ class CreateSheetState extends State<CreateSheet> {
       iconSize: 25.0,
     );
 
-    final name = RadioListTile(
-      value: false,
-      title: Text('Hello'),
-      //onChanged: (),
-    );
-
     final submitButton = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
@@ -85,16 +70,11 @@ class CreateSheetState extends State<CreateSheet> {
         elevation: 5.0,
         onPressed: () {
           uploadData();
-          /* final FormState = _formKey.currentState;
-
-          if (FormState.validate()) {
-            uploadData();
-          }*/
         },
         child: Icon(Icons.check),
       ),
     );
-    
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Create Sheet'),
@@ -106,10 +86,8 @@ class CreateSheetState extends State<CreateSheet> {
                     fontSize: 15.0,
                     color: Colors.white)),
             onPressed: () {
-              //ListPage.uploadData();
-              //print('Done');
-
-              //print(ListPage.presentList);
+              uploadData();
+              
             },
           )
         ],
@@ -153,85 +131,61 @@ class CreateSheetState extends State<CreateSheet> {
             ),
             Flexible(
               child: Container(
-      child: FutureBuilder(
-        future: getPosts(),
-        builder: (_, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: Text("Loading"),
-            );
-          } else {
-            // final nameList = snapshot.data["Name"];
-            //print(nameList);
-            return Column(
-              children: <Widget>[
-                ListView.builder(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (_, index) {
+                child: FutureBuilder(
+                  future: getPosts(),
+                  builder: (_, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
                       return Center(
-                        child: CheckboxListTile(
-                            value: names
-                                .contains(snapshot.data[index].data["Name"]),
-                            title: Text(snapshot.data[index].data["Name"]),
-                            onChanged: (bool selected) {
-                              //markMember(snapshot.data[index].data["Name"]);
-                              _onCategorySelected(
-                                  selected,
-                                  snapshot.data[index].data["Name"],
-                                  snapshot.data[index].data["IndexNo"]);
+                        child: Text("Loading"),
+                      );
+                    } else {
+                      return Center(
+                        child: ListView.builder(
+                            padding: const EdgeInsets.only(bottom: 20.0),
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (_, index) {
+                              return Center(
+                                child: CheckboxListTile(
+                                    value: names.contains(
+                                        snapshot.data[index].data["Name"]),
+                                    title:
+                                        Text(snapshot.data[index].data["Name"]),
+                                    onChanged: (bool selected) {
+                                      _onCategorySelected(
+                                          selected,
+                                          snapshot.data[index].data["Name"],
+                                          snapshot.data[index].data["IndexNo"]);
+                                    }),
+                              );
                             }),
                       );
-                    }),
-                Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 120,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        SizedBox(
-                          width: 290,
-                        ),
-                        submitButton,
-                      ],
-                    ),
-                  ],
+                    }
+                  },
                 ),
-              ],
-            );
-          }
-        },
-      ),
-    ),
+              ),
             ),
           ],
         ),
       )),
     );
   }
-  String getDate(){
+
+  String getDate() {
     return strDate;
   }
-    // crudMethods crudObj = new crudMethods();
+
   void _onCategorySelected(bool selected, name, indexno) {
     if (selected == true) {
       setState(() {
-        //presentMembers.add(category_id);
         names.add(name);
         index.add(indexno);
-        //attendanceSheet.addAll("",names);
-        //presentMembers['Name'] = name;
-        //presentMembers['indexno'] = indexno;
       });
     } else {
       setState(() {
         names.remove(name);
         index.remove(indexno);
-        //presentMembers.remove(name);
-        //presentMembers.remove(indexno);
       });
     }
     print(names);
@@ -250,15 +204,14 @@ class CreateSheetState extends State<CreateSheet> {
       'Names': names,
       'Index': index,
     };
-//var getDate = CreateSheetState();
-//String date = getDate.strDate;
-    //Firestore.instance.collection('teamapp').add(memberDetails).catchError((e){
-    //print(e);
-    //String today = CreateSheet.strDate;
-    //print(today);
-    Firestore.instance.collection('today').document(strDate).setData(attendanceSheet).then((result){
+
+    Firestore.instance
+        .collection('RecodeBook')
+        .document(strDate)
+        .setData(attendanceSheet)
+        .then((result) {
       print(strDate);
-       Fluttertoast.showToast(
+      Fluttertoast.showToast(
           msg: "Done",
           toastLength: Toast.LENGTH_SHORT,
           gravity: ToastGravity.BOTTOM,
@@ -266,16 +219,8 @@ class CreateSheetState extends State<CreateSheet> {
           backgroundColor: Colors.black54,
           textColor: Colors.white,
           fontSize: 16.0);
-    }).catchError((e){
+    }).catchError((e) {
       print(e);
     });
-    /*Firestore.instance
-        .collection('today')
-        .add(attendanceSheet)
-        .catchError((e) {
-      print(e);
-    });*/
   }
-
-
 }
